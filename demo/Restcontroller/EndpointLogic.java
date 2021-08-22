294231284ac74ef92232ef943e4596d525b694f2
ankit1399 <64706180+ankit1399@users.noreply.github.com>
2021-08-22 12:40:15 +0530

    Update EndpointLogic.java

--- demo/Restcontroller/EndpointLogic.java
+++ demo/Restcontroller/EndpointLogic.java
@@ -1,24 +1,15 @@
 package com.example.demo.Restcontroller;
-
 import java.util.ArrayList;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
 public class EndpointLogic {
 	@Autowired
 	private Repo repo;
-	
-    public Teacher getTeacher(int id) {
+        public Teacher getTeacher(int id) {
 		return repo.findById(id).get();
 	}
-    
-	
-	public void postTeacher(Teacher teacher) {
+        public void postTeacher(Teacher teacher) {
 		repo.save(teacher);
-		
-		
 	}
 }
